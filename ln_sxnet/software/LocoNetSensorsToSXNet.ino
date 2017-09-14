ec31239336517acf5bc74a1976a7cc0ebffea545
Michael Blank <mblank@bawue.de>
2017-09-14 17:18:48 +0200

    initial version of LN-Toti

---
+++ ln_sxnet/software/LocoNetSensorsToSXNet.ino
@@ -0,0 +1,197 @@
+/****************************************************************************
+ * 	Copyright (C) 2017 Michael Blank
+ * 
+ * 	This library is free software; you can redistribute it and/or
+ * 	modify it under the terms of the GNU Lesser General Public
+ * 	License as published by the Free Software Foundation; either
+ * 	version 2.1 of the License, or (at your option) any later version.
+ * 
+ * 	This library is distributed in the hope that it will be useful,
+ * 	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * 	Lesser General Public License for more details.
+ * 
+ * 	You should have received a copy of the GNU Lesser General Public
+ * 	License along with this library; if not, write to the Free Software
+ * 	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * 
+ *****************************************************************************
+ * 
+ * Title :   LocoNetSensorsToSXNet
+ * Author:   Michael Blank
+ * Date:     09-Sep-2017
+ * Software: AVR-GCC
+ * Target:   Arduino
+ * 
+ * 
+ * DESCRIPTION
+ *   sends all sensor messages (received from LocoNet) in an address range from
+ *   850 to 1040 to SXnet 85 to 104 
+ *   channel 85, sxbis1 = 850 ...
+ *   channel 85, sxbit8 = 857 etc 
+ * 
+ *   The Loconet RX is on arduino pin 8, the tx on arduino pin 6.
+ *   
+ *   Hardware: Arduino (Uno) Ethernet + FREMO LN-Shield
+ *   
+ *   startup: sensor boards send there state a few seconds after a reset
+ *   
+ *****************************************************************************/
+
+#include <LocoNet.h>
+#include <SPI.h>
+#include <Ethernet.h>
+
+#define DEBUG /* enable this to print debug information via the serial port */
+
+/* This is the TCP port used to communicate over ethernet */
+
+#define TCP_PORT 4104
+#define UPDATE_INTERVAL  200    // send update every 200 msec
+uint32_t lastUpdate = 0;
+
+/* Modify these variables to match your network */
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+byte ip[] = { 192, 168, 178, 100 };
+byte gateway[] = { 192, 168, 178, 1 };
+byte subnet[] = { 255, 255, 255, 0 };
+
+// retry connection, if not connected, after RETRY_INTERVAL milliseconds
+#define RETRY_INTERVAL   30000
+uint32_t lastRetry = 0;
+
+const char digitMap[] = "0123456789abcdef";
+const char receiveString[] = "RECEIVE ";
+static LnBuf LnTxBuffer;
+static lnMsg *LnPacket;
+
+EthernetClient client;
+IPAddress server(192, 168, 178, 29);
+
+#define MIN_SX_ADDR   85
+#define MAX_SX_ADDR   104
+#define LN_MULTIPLIER  10   // LN_ADDR = SX_ADDR + (iBit -1)  88,bit1 == 880
+#define SENSOR_COMM
+byte sxData[MAX_SX_ADDR - MIN_SX_ADDR + 1]; //sx addresses 80 (=[0]) to 104 [24]
+boolean sxChanged[MAX_SX_ADDR - MIN_SX_ADDR + 1]; //sx addresses 80 (=[0]) to 104 [24]
+
+void connectToSXnet() {
+	/* SXnet defaults to PORT 4104 */
+	// if you get a connection, report back via serial:
+	if (client.connect(server, TCP_PORT)) {
+		Serial.println("connected");
+	} else {
+		// kf you didn't get a connection to the server:
+		Serial.println("connection failed");
+	}
+}
+
+String IpAddress2String(const IPAddress& ipAddress) {
+	return String(ipAddress[0]) + String(".") + String(ipAddress[1])
+			+ String(".") + String(ipAddress[2]) + String(".")
+			+ String(ipAddress[3]);
+}
+
+void setup() {
+	/* First initialize the LocoNet interface */
+	LocoNet.init();
+
+	/* Configure the serial port for 57600 baud */
+	Serial.begin(57600);
+	Serial.println(F("LocoNet Sensors -> SXnet started!!"));
+	Serial.print(F("loconet sensor addresses must be in range "));
+	Serial.print(MIN_SX_ADDR * LN_MULTIPLIER);
+	Serial.print(" to ");
+	Serial.println(MAX_SX_ADDR * LN_MULTIPLIER);
+	Serial.print(F("Server address ="));
+	String sIP = IpAddress2String(server);
+	Serial.println(sIP);
+
+	/*Initialize a LocoNet packet buffer to buffer bytes from the PC */
+	initLnBuf(&LnTxBuffer);
+
+	/* initialise the ethernet device */
+	Ethernet.begin(mac, ip, gateway, subnet);
+
+	/* connect */
+	connectToSXnet();
+
+}
+
+void loop() {
+
+	if (client.available()) {
+		char c = client.read();
+		Serial.print(c);
+	}
+
+	/* Check for any received loconet packets */
+	LnPacket = LocoNet.receive();
+	if (LnPacket) {
+
+#ifdef DEBUG
+		/* If a packet has been received, get the length of the 
+		 loconet packet */
+		uint8_t LnPacketSize = getLnMsgSize(LnPacket);
+		//Serial.print("Received a message of ");
+		//Serial.print(LnPacketSize, DEC);
+		//Serial.println(" Bytes from loconet,");
+#endif
+
+		LocoNet.processSwitchSensorMessage(LnPacket);
+	}  // LnPacket
+
+	// update SXnet at most every 200msecs (=UPDATE_INTERVAL)
+	if ((millis() - lastUpdate) > UPDATE_INTERVAL) {
+		lastUpdate = millis();
+		for (uint8_t i = 0; i <= (MAX_SX_ADDR - MIN_SX_ADDR); i++) {
+			if (sxChanged[i]) {
+
+				uint8_t a = i + MIN_SX_ADDR;
+
+				if (client.connected()) {
+					sxChanged[i] = false;
+					client.print("S ");
+					client.print(a);
+					client.print(" ");
+					client.println(sxData[i]);
+#ifdef DEBUG
+					Serial.print("S ");
+					Serial.print(a);
+					Serial.print(" ");
+					Serial.println(sxData[i]);
+#endif					
+				}
+			} // sxChanged[i]
+		} // i 0..N_ADDR
+	}  // update-interval
+
+	// check connection state and try to reconnect, if not connected
+	if (!client.connected()) {
+		Serial.println("disconnected.");
+		client.stop();
+		delay(1000);
+		if ((millis() - lastRetry) > RETRY_INTERVAL) {
+			lastRetry = millis();
+			connectToSXnet();
+		}
+	}
+}
+
+// This call-back function is called from LocoNet.processSwitchSensorMessage
+// for all Sensor messages
+void notifySensor(uint16_t Address, uint8_t State) {
+#ifdef DEBUG
+	Serial.print("Sensor: ");
+	Serial.print(Address, DEC);
+	Serial.print(" - ");
+	Serial.println(State ? "Active" : "Inactive");
+#endif
+	byte sxAddr = Address / LN_MULTIPLIER;
+	byte sxBit = Address - LN_MULTIPLIER * sxAddr;  // sxBit 0 ..7
+	if ((sxAddr >= MIN_SX_ADDR) && (sxAddr <= MAX_SX_ADDR)) {
+		bitWrite(sxData[sxAddr - MIN_SX_ADDR], sxBit, State);
+		sxChanged[sxAddr - MIN_SX_ADDR] = true;
+	}
+}
+
---
+++ ln_toti/software/LocoNetToti/LocoNetToti.ino
@@ -0,0 +1,279 @@
+/*******************************************************************************************
+ * LocoNetToti 
+ * (based on: SX_Belegtmelder_V0210)
+ *
+ * Created:    09.09.2017
+ * Copyright:  Michael Blank
+ * Version:    V01.00
+ * 
+ * Zielhardware/-controller: Arduino Pro Mini (5V, 16MHz), ATmega328
+ * Basis LN 1.0 + BM-Aufsteckplatine R. Thamm
+ * 
+ * Adresse des Moduls und Abfallverzögerung werden im EEPROM des Controllers abgespeichert.
+ * 
+ * Changes:
+ * 09.09.2017:      initial version
+ *
+ *******************************************************************************************/
+#define TESTVERSION  // aktiviert serielle Testausgaben und Dialog über Serial Interface
+//#define DEBUG_TOTI   // aktiviert TOTI Output über Serial
+
+#include <LocoNet.h>
+#include <EEPROM.h>
+#include <OneButton.h>
+
+#define  DEFAULTADDR  880          // die Start-Adresse, auf die dieses Modul hört
+#define  DEFAULTCODEDDELAYTIME 20  // *25ms = 500ms Abfallverzögerung
+#define  DELAYTIMEFACTOR 25        // 1 digit entspricht 25ms
+#define  START_DELAY   3000        // send "start of day" messages 3 secs after reset
+
+#define  TESTTAKT 1000
+#define  LEDTAKT 250
+#define  PROGLED 13
+#define  PROGBUTTON 4   // !! LN Basis Board
+#define  PROG_ACTIVATE_TIME  3000   // Tastendruck für 3 sekunden
+
+#define  EEPROMSIZE 2                  // 2 Byte vorsehen: Adresse und Abfallverzögerung
+
+char swversion[] = "SW V01.01";
+char hwversion[] = "HW V01.00";
+
+int LNAddr;
+lnMsg *LnPacket;
+
+int err;
+
+// Pindefinitionen BASIS SX RJ45-1.1
+//byte DataPin[8] = { 5, 7, 8, 9, 10, 11, 12, A0 };
+
+// Pindefinitionen BASIS LocoNet-1.0
+byte DataPin[8] = { 3, 7, 5, 9, 10, 11, 12, A0 };
+
+byte DataRead[8] = { 0, 0, 0, 0, 0, 0, 0, 0 }; // gelesene Daten
+byte DataOut[8] = { 0, 0, 0, 0, 0, 0, 0, 0 }; // zur Meldung a.d. Zentrale
+byte lastDataOut[8] = { 2, 2, 2, 2, 2, 2, 2, 2 }; // letzte Meldung a.d. Zentrale
+unsigned long lastOccupied[8] = { 0, 0, 0, 0, 0, 0, 0, 0 }; // Zeitpunkt letzter Belegung
+
+unsigned delayTime;               // Verzögerungszeit gegen Melderflackern
+int codedDelayTime;       // Abbildung in einem Byte: pro Digit=25ms, hier 500ms
+//   dieser Wert wird im EEPROM abgelegt
+
+// für den Programmiervorgang über den LN-Bus:
+boolean programming = false;
+OneButton progBtn(PROGBUTTON, true);  // 0 = key pressed
+
+unsigned long time1;
+
+
+byte finalData;
+
+//******************************************************************************************
+void setup() {
+
+	LocoNet.init();
+
+
+	time1 = millis();
+	Serial.begin(57600);
+	delay(10);
+	Serial.println(swversion);
+	Serial.println(hwversion);
+
+
+	for (int i = 0; i < 8; i++) {
+		pinMode(DataPin[i], INPUT);
+		digitalWrite(DataPin[i], HIGH);
+		lastOccupied[i] = millis();
+	}
+
+	digitalWrite(PROGLED, LOW);
+	pinMode(PROGLED, OUTPUT);
+
+	progBtn.attachLongPressStart(toggleProgramming);
+	progBtn.setPressTicks(PROG_ACTIVATE_TIME);
+
+  LNAddr = DEFAULTADDR;
+  delayTime = DEFAULTCODEDDELAYTIME * DELAYTIMEFACTOR;
+  
+	/*if (!EEPROMEmpty()) {
+	 EEPROMRead();
+	 }
+	 else {
+	 SXAddr = DEFAULTADDR;
+	 codedDelayTime = DEFAULTCODEDDELAYTIME;
+	 delayTime = codedDelayTime * DELAYTIMEFACTOR;
+	 EEPROMSave();
+	 } */
+
+	// TODO init LocoNet
+}
+
+
+
+//******************************************************************************************
+void toggleProgramming() {
+	// called when button is long pressed
+#ifdef TESTVERSION
+	Serial.println(F("toggleProgramming"));
+#endif
+
+	if (!programming) {
+		startProgramming();
+	} else {
+		finishProgramming();
+	}
+}
+
+//******************************************************************************************
+void startProgramming() {
+
+#ifdef TESTVERSION
+	Serial.println(F("startProgramming"));
+#endif 
+	digitalWrite(PROGLED, HIGH);
+	programming = true;
+	//saveOldSXValues();
+
+	// TODO
+}
+
+//******************************************************************************************
+void finishProgramming() {
+
+#ifdef TESTVERSION
+	Serial.println(F("finishProgramming"));
+#endif
+	/*SXAddr         = checkedSXtmp[0];
+	 codedDelayTime = checkedSXtmp[1];
+
+	 EEPROMSave();
+	 EEPROMRead();
+	 restoreOldSXValues();
+	 delay(80); */
+	programming = false;
+	digitalWrite(PROGLED, LOW);
+}
+
+//******************************************************************************************
+void processProgramming() {
+// TODO
+}
+
+/*** LOCONET PROTOCOL:
+ *   [B2 61 72 5E]  Sensor LS708 () is High    
+ *   [B2 61 62 4E]  Sensor LS708 () is Low
+ *   <IN1> => A6=1, A5=1, A0=1 => addr1 = 1 + 32 + 64 = 97
+ *   <IN2> => A8=1             => addr2 = 256  => LN_ADDR = 353
+ *   
+ *   OPC_INPUT_REP  0xB2
+; General SENSOR Input codes
+; <0xB2>, <IN1>, <IN2>, <CHK>
+
+<IN1> =<0,A6,A5,A4 - A3,A2,A1,A0>, 7 ls adr bits. A1,A0 select 1 of 4 inputs pairs in a DS54
+<IN2> =<0,X,I,L - A10,A9,A8,A7>
+Report/status bits and 4 MS adr bits.
+"I"=0 for DS54 "aux" inputs and 1 for "switch" inputs mapped to 4K SENSOR space.
+(This is effectively a least significant adr bit when using DS54 input configuration)
+"L"=0 for input SENSOR now 0V (LO) , 1 for Input sensor >=+6V (HI), "X"=1, control bit , 
+                                              0 is RESERVED for future!
+ */
+
+//******************************************************************************************
+void loop() {
+
+	int x;
+
+	progBtn.tick();
+	if (programming) {            // process the received data from SX-bus
+		processProgramming();
+	}
+
+	for (int i = 0; i < 8; i++) {
+		DataRead[i] = digitalRead(DataPin[i]); // active low Input: Data[i]==0 = Belegt!
+		if (!DataRead[i]) { // --> belegt!
+			DataOut[i] = DataRead[i];
+			lastOccupied[i] = millis();
+		} else {            // frei!
+			if (millis() > lastOccupied[i] + delayTime) { // delayTime abgelaufen?
+				DataOut[i] = DataRead[i];
+			}
+		}
+    if (millis() > START_DELAY) { // send SOD after 3 secs after reset
+                                 // after network interface has been started
+		if (DataOut[i] != lastDataOut[i]) {
+			lastDataOut[i] = DataOut[i];
+      if (DataOut[i]) {
+        LocoNet.reportSensor((uint16_t)(LNAddr+i), 0);
+      } else {
+        LocoNet.reportSensor((uint16_t)(LNAddr+i), 1);
+      }
+		}
+   }
+
+	}
+
+	lnMsg *LnPacket;
+	// TODO send to LN Bus
+
+	LnPacket = LocoNet.receive();
+
+	if (LnPacket) {
+
+		unsigned char opcode = (int) LnPacket->sz.command;
+
+		// First print out the packet in HEX
+		Serial.print("RX: ");
+		uint8_t msgLen = getLnMsgSize(LnPacket);
+		for (uint8_t x = 0; x < msgLen; x++) {
+			uint8_t val = LnPacket->data[x];
+			// Print a leading 0 if less than 16 to make 2 HEX digits
+			if (val < 16)
+				Serial.print('0');
+
+			Serial.print(val, HEX);
+			Serial.print(' ');
+		}
+
+   // If this packet was not a Switch or Sensor Message then print a new line
+    if (!LocoNet.processSwitchSensorMessage(LnPacket)) {
+      Serial.println();
+    }
+
+	}
+
+
+#ifdef DEBUG_TOTI
+	if (millis() > time1 + TESTTAKT) {
+		time1 = millis();
+
+		for (int i = 7; i >= 0; i--) {
+			Serial.print(bitRead(finalData, i));
+		}
+		Serial.print("  ");
+		Serial.println(finalData);
+	}
+
+#endif
+}   //loop
+
+// This call-back function is called from LocoNet.processSwitchSensorMessage
+// for all Sensor messages
+void notifySensor( uint16_t Address, uint8_t State ) {
+  Serial.print("notify Sensor: ");
+  Serial.print(Address, DEC);
+  Serial.print(" - ");
+  Serial.println( State ? "Active" : "Inactive" );
+}
+
+// This call-back function is called from LocoNet.processSwitchSensorMessage
+// for all Switch Sensor Report messages
+void notifySwitchReport( uint16_t Address, uint8_t State, uint8_t Sensor ) {
+  Serial.print("Switch Sensor Report: ");
+  Serial.print(Address, DEC);
+  Serial.print(':');
+  Serial.print(Sensor ? "Switch" : "Aux");
+  Serial.print(" - ");
+  Serial.println( State ? "Active" : "Inactive" );
+}
+
+
